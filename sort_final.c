41d0f57c48e478f936c014b097d08ee52c4a878b
bobbykemp <robert.kemp@mavs.uta.edu>
2018-10-07 11:34:02 -0500

    adding the final sort with 1 and 2 process sort completed

---
+++ sort_final.c
@@ -0,0 +1,464 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+typedef struct{
+
+    char date[2048];
+    char season[5];
+    int neutral;
+    char playoff[6];
+    char team1[5];
+    char team2[5];
+    float elo1_pre;
+    float elo2_pre;
+    float elo_prob1;
+    float elo_prob2;
+    float elo1_post;
+    float elo2_post;
+    float rating1_pre;
+    float rating2_pre;
+    char pitcher1 [2048];
+    char pitcher2 [2048];
+    float pitcher1_rgs;
+    float pitcher2_rgs;
+    float pitcher1_adj;
+    float pitcher2_adj;
+    float rating_prob1;
+    float rating_prob2;
+    float rating1_post;
+    float rating2_post;
+    int score1;
+    int score2;
+
+}entry;
+
+void* create_shared_mem(size_t size){
+
+  int protection = PROT_READ | PROT_WRITE;
+
+  int visibility = MAP_ANONYMOUS | MAP_SHARED;
+
+  return mmap(NULL, size, protection, visibility, 0, 0);
+}
+
+void merge_arr(entry *res_arr, entry *arr1, entry *arr2, int len1, int len2){
+
+    int total = len1 + len2;
+
+    int l_ptr = 0, r_ptr = 0;
+
+    for (int i = 0; i < total; i++)
+    {
+        if(l_ptr < len1 && r_ptr < len2){
+            // printf("-----------\n");
+            // printf("Round %d\n", i);
+            if(strcmp(arr1[l_ptr].team1, arr2[r_ptr].team1) <= 0){
+                // printf("cond 1\n");
+                // printf("%s comes before %s and %d < %d\n", arr1[l_ptr].team1, arr2[r_ptr].team1, l_ptr, len1);
+
+                // printf("putting %s in slot %d\n", arr1[l_ptr].team1, i);
+                res_arr[i] = arr1[l_ptr];
+
+                l_ptr++;
+
+            }
+            else{
+                // printf("cond 2\n");
+                // printf("%s comes after %s and %d < %d\n", arr1[l_ptr].team1, arr2[r_ptr].team1, r_ptr, len2);
+
+                // printf("putting %s in slot %d\n", arr2[r_ptr].team1, i);
+                res_arr[i] = arr2[r_ptr];
+
+                r_ptr++;
+
+            }
+
+        }
+        else{
+            if(l_ptr == len1){
+                for (int j = r_ptr; j < len2; ++j)
+                {
+                    printf("dumping %s into slot %d\n", arr2[j].team1, i);
+                    res_arr[i++] = arr2[j];
+                }
+            }
+            else if(r_ptr == len2){
+                for (int j = l_ptr; j < len1; ++j)
+                {
+                    printf("dumping %s into slot %d\n", arr1[j].team1, i);
+                    res_arr[i++] = arr1[j];
+                }
+            }
+        }
+
+    }
+}
+
+void swap_elem(entry *a, entry *b){
+    entry temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void split(entry *arr, entry *arr1, entry *arr2, int arr1_len, int arr_2len){
+    for (int i = 0; i < arr1_len; ++i)
+    {
+        arr1[i] = arr[i];
+    }
+    for (int i = 0; i < arr_2len; ++i)
+    {
+        arr2[i] = arr[arr1_len + i];
+    }
+}
+
+void puke(entry entries[], int num_lines){
+    for (int i = 0; i < num_lines; ++i)
+    {
+        // printf("index is %d\n", i);
+        // printf("---------------------------------\n");
+        // printf("date is %s\n", entries[i].date);
+        // printf("season is %s\n", entries[i].season);
+        // printf("neutral is %d\n", entries[i].neutral);
+        // printf("playoff is %s\n", entries[i].playoff);
+        printf("%d: %s\n", i, entries[i].team1);
+        // printf("team2 is %s\n", entries[i].team2);
+        // printf("elo1_pre is %f\n", entries[i].elo1_pre);
+        // printf("elo2_pre is %f\n", entries[i].elo2_pre);
+        // printf("elo_prob1 is %f\n", entries[i].elo_prob1);
+        // printf("elo_prob2 is %f\n", entries[i].elo_prob2);
+        // printf("elo1_post is %f\n", entries[i].elo1_post);
+        // printf("elo2_post is %f\n", entries[i].elo2_post);
+        // printf("rating1_pre is %f\n", entries[i].rating1_pre);
+        // printf("rating2_pre is %f\n", entries[i].rating2_pre);
+        // printf("pitcher1 is %s\n", entries[i].pitcher1);
+        // printf("pitcher2 is %s\n", entries[i].pitcher2);
+        // printf("pitcher1_rgs is %f\n", entries[i].pitcher1_rgs);
+        // printf("pitcher2_rgs is %f\n", entries[i].pitcher2_rgs);
+        // printf("pitcher1_adj is %f\n", entries[i].pitcher1_adj);
+        // printf("pitcher2_adj is %f\n", entries[i].pitcher2_adj);
+        // printf("rating_prob1 is %f\n", entries[i].rating_prob1);
+        // printf("rating_prob2 is %f\n", entries[i].rating_prob2);
+        // printf("rating1_post is %f\n", entries[i].rating1_post);
+        // printf("rating2_post is %f\n", entries[i].rating2_post);
+        // printf("score1 is %d\n", entries[i].score1);
+        // printf("score2 is %d\n", entries[i].score2);
+    }
+}
+
+void bubbly(entry *arr, int end){
+    printf("entered bubbly\n");
+    for (int i = 0; i < end - 1; ++i)
+    {
+        for (int j = 0; j < (end - i - 1); ++j)
+        {
+            if(strcmp(arr[j].team1, arr[j + 1].team1) >= 0){
+
+                swap_elem(&arr[j], &arr[j + 1]);
+
+            }
+        }
+    }
+}
+
+const int size = 13;
+
+int main(int argc, char const *argv[])
+{
+
+    FILE *file = fopen(argv[1], "r");
+    char *value_buffer;
+
+    if(argv[1] == "" || argv[1] == NULL){
+        printf("You need to supply at least one argument. Exiting\n");
+        exit(-1);
+    }
+
+    char c;
+    entry *entries; //dynamic array of entries
+    int num_lines = 0;
+
+    char *buf;
+
+    size_t size = 99999;
+
+    c = getc(file);
+
+    while(c != EOF){
+        if(c == '\n'){
+            num_lines++;
+        }
+        c = getc(file);
+    }
+
+    printf("There are %d lines\n", num_lines);
+
+    entries = malloc(num_lines * sizeof(entry));
+    buf = malloc(size * sizeof(char));
+
+    if(fseek(file, 0, SEEK_SET)){printf("Failed to seek, exiting\n"); exit(-1);}
+
+    const char delim = ',';
+
+    for (int i = 0; i < num_lines; ++i)
+    {
+        //put a line in the buffer
+        buf = NULL;
+        // printf("At index %d\n", i);
+        getline(&buf, &size, file);
+
+        strcpy(entries[i].date, strsep(&buf, &delim));
+        strcpy(entries[i].season, strsep(&buf, &delim));
+        entries[i].neutral = atoi(strsep(&buf, &delim));
+        strcpy(entries[i].playoff, strsep(&buf, &delim));
+        strcpy(entries[i].team1, strsep(&buf, &delim));
+        strcpy(entries[i].team2, strsep(&buf, &delim));
+        entries[i].elo1_pre = atof(strsep(&buf, &delim));
+        entries[i].elo2_pre = atof(strsep(&buf, &delim));
+        entries[i].elo_prob1 = atof(strsep(&buf, &delim));
+        entries[i].elo_prob2 = atof(strsep(&buf, &delim));
+        entries[i].elo1_post = atof(strsep(&buf, &delim));
+        entries[i].elo2_post = atof(strsep(&buf, &delim));
+        entries[i].rating1_pre = atof(strsep(&buf, &delim));
+        entries[i].rating2_pre = atof(strsep(&buf, &delim));
+        strcpy(entries[i].pitcher1, strsep(&buf, &delim));
+        strcpy(entries[i].pitcher2, strsep(&buf, &delim));
+        entries[i].pitcher1_rgs = atof(strsep(&buf, &delim));
+        entries[i].pitcher2_rgs = atof(strsep(&buf, &delim));
+        entries[i].pitcher1_adj = atof(strsep(&buf, &delim));
+        entries[i].pitcher2_adj = atof(strsep(&buf, &delim));
+        entries[i].rating_prob1 = atof(strsep(&buf, &delim));
+        entries[i].rating_prob2 = atof(strsep(&buf, &delim));
+        entries[i].rating1_post = atof(strsep(&buf, &delim));
+        entries[i].rating2_post = atof(strsep(&buf, &delim));
+        entries[i].score1 = atoi(strsep(&buf, &delim));
+        entries[i].score2 = atoi(strsep(&buf, &delim));
+    }
+
+    printf("How many processes should be run (1, 2, 4, or 8?)\n");
+
+    int choice = 0;
+
+    while(choice != 1 && choice != 2 /*&& choice != 4 && choice != 8*/){
+        scanf("%d", &choice);
+    }
+
+    if (choice == 1){
+
+        time_t start = time(NULL);
+
+        printf("array pre-sort:\n");
+        puke(entries, num_lines);
+
+        bubbly(entries, num_lines);
+
+        printf("array post-sort:\n");
+        puke(entries, num_lines);
+
+        time_t end = time(NULL);
+
+        time_t delta = end - start;
+
+        printf("Sort took %ld seconds\n", delta);
+    }
+
+    if(choice == 2){
+
+        time_t start = time(NULL);
+
+        //split original array
+        int len = num_lines;
+        int mid = len / 2;
+        int rem = len - mid;
+
+        entry *arr1 = create_shared_mem(mid * sizeof(entry));
+        entry *arr2 = create_shared_mem(rem * sizeof(entry));
+
+        split(entries, arr1, arr2, mid, rem);
+
+        printf("array 1 is\n");
+        puke(arr1, mid);
+        printf("array 2 is\n");
+        puke(arr2, rem);
+
+        // puke(arr1, mid);
+        // puke(arr2, rem);
+
+        printf("-------------------\n");
+
+        int pid = fork();
+
+        if(pid < 0){
+            perror("fork failed");
+            exit(-1);
+        }
+
+        else if(pid == 0){
+
+            printf("sorting in child\n");
+
+            bubbly(arr1, mid);
+
+            exit(1);
+        }
+
+        else if(pid > 0){
+
+            printf("sorting in parent\n");
+
+            bubbly(arr2, rem);
+
+            wait(NULL);
+        }
+
+        printf("array 1 post sort:\n");
+        puke(arr1, mid);
+        printf("array 2 post sort:\n");
+        puke(arr2, rem);
+
+        entry *res_arr = malloc(num_lines * sizeof(entry));
+
+        printf("mid before merge is : %d\nrem before merge is: %d\n", mid, rem);
+
+        merge_arr(res_arr, arr1, arr2, mid, rem);
+
+        time_t end = time(NULL);
+
+        time_t delta = end - start;
+
+        puke(res_arr, num_lines);
+
+        printf("Sort took %ld seconds\n", delta);
+
+        free(res_arr);
+    }
+
+    /*----------------------------------------------*/
+
+    /*if(choice == 4){
+
+        //split first half of new array
+        int len2 = mid;
+        // printf("len is %d\n", len);
+        int mid2 = len2 / 2;
+        // printf("mid is %d\n", mid);
+        int rem2 = len2 - mid2;
+        // printf("rem is %d\n", rem);
+
+        int *arr3 = create_shared_mem(mid2 * sizeof(int));
+        int *arr4 = create_shared_mem(rem2 * sizeof(int));
+
+        split(arr1, arr3, arr4, mid2, rem2);
+
+        printf("array 3 is:\n");
+        puke(arr3, mid2);
+        printf("array 4 is:\n");
+        puke(arr4, rem2);*/
+
+        /*----------------------------------------------*/
+
+         //split second half of new array
+       /* int len3 = rem;
+        int mid3 = len3 / 2;
+        int rem3 = len3 - mid3;
+
+        int *arr5 = create_shared_mem(mid3 * sizeof(int));
+        int *arr6 = create_shared_mem(rem3 * sizeof(int));
+
+        split(arr2, arr5, arr6, mid3, rem3);
+
+        printf("array 5 is:\n");
+        puke(arr5, mid3);
+        printf("array 6 is:\n");
+        puke(arr6, rem3);
+
+        printf("---------------\n");
+
+    }*/
+
+    // puke(arr1, mid);
+    // puke(arr2, rem);
+
+    // bubbly(arr1, mid);
+    // bubbly(arr2, rem);
+
+    /*if(choice == 4){
+
+        for (int i = 0; i < choice; ++i)
+        {
+            pid = fork();
+
+            if(pid < 0){
+                perror("fork failed");
+                exit(-1);
+            }
+
+            else if(pid == 0){
+
+                    if(i == 0){
+                        bubbly(arr3, mid2);
+                    }
+                    else if (i == 1){
+                        bubbly(arr4, rem2);
+                    }
+                    else if (i == 2){
+                        bubbly(arr5, mid3);
+                    }
+                    else{
+                        bubbly(arr6, rem3);
+                    }
+
+                exit(1);
+            }
+
+            else if(pid > 0){
+                if(choice == 2){
+                    bubbly(arr2, rem);
+                }
+                wait(NULL);
+            }
+        }
+    }*/
+
+
+        
+
+    /*printf("first array after sort:\n");
+    puke(arr3, mid2);
+
+    printf("second array after sort:\n");
+    puke(arr4, rem2);
+
+    printf("third array after sort:\n");
+    puke(arr5, mid3);
+
+    printf("fourth array after sort:\n");
+    puke(arr6, rem3);*/
+
+    //mid is length of arr1, rem is length of arr2
+
+    /*int *res_arr1 = malloc((mid2 + rem2) * sizeof(int));
+
+    merge_arr(res_arr1, arr3, arr4, mid2, rem2);
+
+    int *res_arr2 = malloc((mid3 + rem3) * sizeof(int));
+
+    merge_arr(res_arr2, arr5, arr6, mid3, rem3);
+
+    int *fin_arr = malloc((mid2 + rem2 + mid3 + rem3) * sizeof(int));
+
+    merge_arr(fin_arr, res_arr1, res_arr2, (mid2 + rem2), (mid3 + rem3));
+
+    printf("final array\n");
+    puke(fin_arr, (mid2 + rem2 + mid3 + rem3));*/
+
+    return 0;
+}
--- test_sort.c
+++ test_sort.c
@@ -28,21 +28,21 @@ void merge_arr(int *res_arr, int *arr1, int *arr2, int len1, int len2){
     for (int i = 0; i < total; i++)
     {
         if(l_ptr < len1 && r_ptr < len2){
-            printf("-----------\n");
-            printf("Round %d\n", i);
+            // printf("-----------\n");
+            // printf("Round %d\n", i);
             if(arr1[l_ptr] < arr2[r_ptr]){
-                printf("%d < %d and %d < %d\n", arr1[l_ptr], arr2[r_ptr], l_ptr, len1);
+                // printf("%d < %d and %d < %d\n", arr1[l_ptr], arr2[r_ptr], l_ptr, len1);
 
-                printf("putting %d in slot %d\n", arr1[l_ptr], i);
+                // printf("putting %d in slot %d\n", arr1[l_ptr], i);
                 res_arr[i] = arr1[l_ptr];
 
                 l_ptr++;
 
             }
             else{
-                printf("%d >= %d and %d < %d\n", arr1[l_ptr], arr2[r_ptr], r_ptr, len2);
+                // printf("%d >= %d and %d < %d\n", arr1[l_ptr], arr2[r_ptr], r_ptr, len2);
 
-                printf("putting %d in slot %d\n", arr2[r_ptr], i);
+                // printf("putting %d in slot %d\n", arr2[r_ptr], i);
                 res_arr[i] = arr2[r_ptr];
 
                 r_ptr++;
@@ -54,14 +54,14 @@ void merge_arr(int *res_arr, int *arr1, int *arr2, int len1, int len2){
             if(l_ptr == len1){
                 for (int j = r_ptr; j < len2; ++j)
                 {
-                    printf("dumping %d into slot %d\n", arr2[j], i);
+                    // printf("dumping %d into slot %d\n", arr2[j], i);
                     res_arr[i++] = arr2[j];
                 }
             }
             else if(r_ptr == len2){
                 for (int j = l_ptr; j < len1; ++j)
                 {
-                    printf("dumping %d into slot %d\n", arr1[j], i);
+                    // printf("dumping %d into slot %d\n", arr1[j], i);
                     res_arr[i++] = arr1[j];
                 }
             }
@@ -115,84 +115,207 @@ const int size = 13;
 int main(int argc, char const *argv[])
 {
 
-    int arr[] = {9, 2, 1, 8, 8, 5, 3, 5, 1, 5, 7, 11, 99};
+    int arr[] = {9, 2, 1, 8, 8, 5, 55, 3, 5, 1, 5, 7, 11, 99};
 
-    int len = sizeof(arr) / sizeof(int);
+    printf("How many processes should be run (1, 2, 4, or 8?)\n");
 
-    int mid = len / 2;
-    int rem = len - mid;
+    int choice = 0;
 
-    printf("mid = %d\n", mid);
-    printf("rem = %d\n", rem);
-
-    int *arr1 = malloc(mid * sizeof(int));
-    int *arr2 = malloc(rem * sizeof(int));
-
-    // int *arr2 = arr + mid;
-
-    // printf("arr2[0] = %d\n", arr2[0]);
-
-    //mid = len(arr1); rem = len(arr2)
-
-    // void* shmem = create_shared_mem(mid * sizeof(arr));
-
-    // memcpy(shmem, arr, sizeof(arr));
-
-    split(arr, arr1, arr2, mid, len);
-
-    // printf("result is %d\n", mid);
-    // printf("remainder is %d\n", rem);
-    // printf("value of arr + mid = %d\n", *arr2);
-
-    puke(arr1, mid);
-    puke(arr2, rem);
-
-    int pid;
-
-    bubbly(arr1, mid);
-    bubbly(arr2, rem);
-
-    /*pid = fork();
-
-    if(pid < 0){
-        perror("fork failed");
-        exit(1);
+    while(choice != 1 && choice != 2 && choice != 4 && choice != 8){
+        scanf("%d", &choice);
     }
 
-    else if(pid == 0){
+    printf("choice = %d\n", choice);
+    exit(1);
 
-        printf("sorting in child\n");
+    if(choice == 2 || choice == 4){
+        //split original array
+        int len = sizeof(arr) / sizeof(int);
+        int mid = len / 2;
+        int rem = len - mid;
 
-        // bubbly(arr1, mid);
-        // memcpy(shmem, arr, sizeof(arr));
+        int *arr1 = create_shared_mem(mid * sizeof(int));
+        int *arr2 = create_shared_mem(rem * sizeof(int));
 
-        exit(1);
+        split(arr, arr1, arr2, mid, rem);
+
+        // puke(arr1, mid);
+        // puke(arr2, rem);
+
+        printf("-------------------\n");
     }
 
-    else if(pid > 0){
+    /*----------------------------------------------*/
 
-        printf("sorting in parent\n");
+    if(choice == 4){
 
-        // bubbly(arr2, rem);
+        //split first half of new array
+        int len2 = mid;
+        // printf("len is %d\n", len);
+        int mid2 = len2 / 2;
+        // printf("mid is %d\n", mid);
+        int rem2 = len2 - mid2;
+        // printf("rem is %d\n", rem);
 
-        wait(NULL);
-    }*/
+        int *arr3 = create_shared_mem(mid2 * sizeof(int));
+        int *arr4 = create_shared_mem(rem2 * sizeof(int));
+
+        split(arr1, arr3, arr4, mid2, rem2);
+
+        printf("array 3 is:\n");
+        puke(arr3, mid2);
+        printf("array 4 is:\n");
+        puke(arr4, rem2);
+
+        /*----------------------------------------------*/
+
+         //split second half of new array
+        int len3 = rem;
+        int mid3 = len3 / 2;
+        int rem3 = len3 - mid3;
+
+        int *arr5 = create_shared_mem(mid3 * sizeof(int));
+        int *arr6 = create_shared_mem(rem3 * sizeof(int));
+
+        split(arr2, arr5, arr6, mid3, rem3);
+
+        printf("array 5 is:\n");
+        puke(arr5, mid3);
+        printf("array 6 is:\n");
+        puke(arr6, rem3);
+
+        printf("---------------\n");
+
+    }
+
+    // puke(arr1, mid);
+    // puke(arr2, rem);
+
+    int pid, pid1;
+
+    // bubbly(arr1, mid);
+    // bubbly(arr2, rem);
+
+    if(choice == 4){
+
+        for (int i = 0; i < choice; ++i)
+        {
+            pid = fork();
+
+            if(pid < 0){
+                perror("fork failed");
+                exit(-1);
+            }
+
+            else if(pid == 0){
+
+                    if(i == 0){
+                        bubbly(arr3, mid2);
+                    }
+                    else if (i == 1){
+                        bubbly(arr4, rem2);
+                    }
+                    else if (i == 2){
+                        bubbly(arr5, mid3);
+                    }
+                    else{
+                        bubbly(arr6, rem3);
+                    }
+
+                exit(1);
+            }
+
+            else if(pid > 0){
+                if(choice == 2){
+                    bubbly(arr2, rem);
+                }
+                wait(NULL);
+            }
+        }
+    }
+
+    if(choice == 2){
+
+        if(pid < 0){
+            perror("fork failed");
+            exit(-1);
+        }
+
+        else if(pid == 0){
+
+            printf("sorting in child\n");
+
+                bubbly(arr3, mid2);
+
+                pid1 = fork();
+
+                if(pid < 0){
+                    perror("fork failed");
+                    exit(-1);
+                }
+
+                else if(pid == 0){
+                    printf("sorting in second child\n");
+
+                    bubbly(arr4, rem2);
+                }
+
+                else if(pid > 0){
+                    wait(NULL);
+                }
+
+            bubbly(arr5, mid3);
+
+
+            exit(1);
+        }
+
+        else if(pid > 0){
+
+            printf("sorting in parent\n");
+
+            bubbly(arr6, rem3);
+
+            wait(NULL);
+        }
+    }
 
     printf("first array after sort:\n");
-    puke(arr1, mid);
+    puke(arr3, mid2);
 
     printf("second array after sort:\n");
-    puke(arr2, rem);
+    puke(arr4, rem2);
+
+    printf("third array after sort:\n");
+    puke(arr5, mid3);
+
+    printf("fourth array after sort:\n");
+    puke(arr6, rem3);
 
     //mid is length of arr1, rem is length of arr2
 
-    int *res_arr = malloc(len * sizeof(int));
+    /*int *res_arr = malloc(len * sizeof(int));
 
     merge_arr(res_arr, arr1, arr2, mid, rem);
 
     puke(res_arr, len);
 
-    free(res_arr);
+    free(res_arr);*/
+
+    int *res_arr1 = malloc((mid2 + rem2) * sizeof(int));
+
+    merge_arr(res_arr1, arr3, arr4, mid2, rem2);
+
+    int *res_arr2 = malloc((mid3 + rem3) * sizeof(int));
+
+    merge_arr(res_arr2, arr5, arr6, mid3, rem3);
+
+    int *fin_arr = malloc((mid2 + rem2 + mid3 + rem3) * sizeof(int));
+
+    merge_arr(fin_arr, res_arr1, res_arr2, (mid2 + rem2), (mid3 + rem3));
+
+    printf("final array\n");
+    puke(fin_arr, (mid2 + rem2 + mid3 + rem3));
 
     return 0;
 }
